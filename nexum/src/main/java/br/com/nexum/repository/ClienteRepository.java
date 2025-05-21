159577b9415fb4d4a46167a37ded8dc7449eb301
Rafael Teixeira <rafaelteixeiravl@gmail.com>
2025-05-21 11:19:12 -0300

    first commit

---
+++ nexum/src/main/java/br/com/nexum/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.nexum.repository;
+
+import com.nexum.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+}
